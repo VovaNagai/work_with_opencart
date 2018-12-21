149856feb5b42bc076f236edbeb9811da44263d6
Vova Nagai <ghostfalcon.kr@gmail.com>
2018-12-21 21:57:36 +0200

    first add language add currency

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://oclocal/admin/');
+define('HTTP_CATALOG', 'http://oclocal/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://oclocal/admin/');
+define('HTTPS_CATALOG', 'http://oclocal/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/Users/ghost/OSPanel/domains/ocLocal/admin/');
+define('DIR_SYSTEM', 'C:/Users/ghost/OSPanel/domains/ocLocal/system/');
+define('DIR_IMAGE', 'C:/Users/ghost/OSPanel/domains/ocLocal/image/');
+define('DIR_LANGUAGE', 'C:/Users/ghost/OSPanel/domains/ocLocal/admin/language/');
+define('DIR_TEMPLATE', 'C:/Users/ghost/OSPanel/domains/ocLocal/admin/view/template/');
+define('DIR_CONFIG', 'C:/Users/ghost/OSPanel/domains/ocLocal/system/config/');
+define('DIR_CACHE', 'C:/Users/ghost/OSPanel/domains/ocLocal/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/Users/ghost/OSPanel/domains/ocLocal/system/storage/download/');
+define('DIR_LOGS', 'C:/Users/ghost/OSPanel/domains/ocLocal/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/Users/ghost/OSPanel/domains/ocLocal/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/Users/ghost/OSPanel/domains/ocLocal/system/storage/upload/');
+define('DIR_CATALOG', 'C:/Users/ghost/OSPanel/domains/ocLocal/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'oc_admin');
+define('DB_PASSWORD', 'NM12');
+define('DB_DATABASE', 'oclocal');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://oclocal/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://oclocal/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/Users/ghost/OSPanel/domains/ocLocal/catalog/');
+define('DIR_SYSTEM', 'C:/Users/ghost/OSPanel/domains/ocLocal/system/');
+define('DIR_IMAGE', 'C:/Users/ghost/OSPanel/domains/ocLocal/image/');
+define('DIR_LANGUAGE', 'C:/Users/ghost/OSPanel/domains/ocLocal/catalog/language/');
+define('DIR_TEMPLATE', 'C:/Users/ghost/OSPanel/domains/ocLocal/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/Users/ghost/OSPanel/domains/ocLocal/system/config/');
+define('DIR_CACHE', 'C:/Users/ghost/OSPanel/domains/ocLocal/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/Users/ghost/OSPanel/domains/ocLocal/system/storage/download/');
+define('DIR_LOGS', 'C:/Users/ghost/OSPanel/domains/ocLocal/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/Users/ghost/OSPanel/domains/ocLocal/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/Users/ghost/OSPanel/domains/ocLocal/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'oc_admin');
+define('DB_PASSWORD', 'NM12');
+define('DB_DATABASE', 'oclocal');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
